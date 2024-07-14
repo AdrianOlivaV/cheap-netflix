8e593c002c1ee786f535dd24afab2cba9dc1a985
AdrianOlivaV <jao.vejar@gmail.com>
2024-07-14 10:31:12 -0700

    añadiendo la libreria Gson

--- src/com/Aluracursos/cheapNetflix/Principal/PrincipalConBuqueda.java
+++ src/com/Aluracursos/cheapNetflix/Principal/PrincipalConBuqueda.java
@@ -26,6 +26,15 @@ public class PrincipalConBuqueda {
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
+//
+//        Lo que es una API y su funcionamiento básico;
+//        Cómo funciona la API de OMDb para buscar películas;
+//        Realizar una consulta en la API de OMDb usando Postman;
+//        Cómo integrarse con la API de OMDb en Java, utilizando las clases HttpClient, HttpRequest y HttpResponse.
+       
+
+
+
 
     }
 }
